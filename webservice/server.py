2cde32fcfe64b55d5ec3183faae92cbc4f53e6bf
akshayajkumar <145012137+akshayajkumar@users.noreply.github.com>
2024-08-06 18:15:09 +0530

    Update server.py

--- webservice/server.py
+++ webservice/server.py
@@ -1,164 +1,160 @@
-
-# IMPORTS DE LIBS PROPRIAS
+# IMPORTS OF OWN LIBRARIES
 from database.db_connect import drop_db, create_db, add_user_and_passw, check_user_and_passw, get_user_id
-from knn_sdk.ClassificadorKNN import Classificador
+from knn_sdk.KNNClassifier import Classifier
 import datetime
 
 import csv
 from flask import Flask, render_template, request, jsonify, url_for
 
-
-TYPING_DATA_PATH = './database/biometria.csv'
-LOG_NAME = 'resultados.log'
+# Paths and Constants
+TYPING_DATA_PATH = './database/keystroke.csv'
+LOG_NAME = 'results.log'
 K = 1
 SPLIT = 0.8
 app = Flask(__name__, static_folder='./static')
 
 @app.route('/')
 def home():
-	return render_template('./home/home.html')
+    return render_template('./home/home.html')
 
-@app.route('/cadastro', methods = ['GET', 'POST'])
-def cadastro():
-	if request.method == 'GET':
-		return render_template('./cadastro/cadastro.html')
-	elif request.method == 'POST':
-		response = dict(request.get_json())
-		username  = response['username']
-		password = response['password']
-		id, result = add_user_and_passw(username, password)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('./register/register.html')
+    elif request.method == 'POST':
+        response = dict(request.get_json())
+        username = response['username']
+        password = response['password']
+        id, result = add_user_and_passw(username, password)
 
-		if result:
-			return jsonify({'cadastro_cod': 'UserRegistrySuccess', 'id_usuario': id})
-		else:
-			return jsonify({'cadastro_cod': 'UsernameAlreadyExist'})
+        if result:
+            return jsonify({'register_code': 'UserRegistrySuccess', 'user_id': id})
+        else:
+            return jsonify({'register_code': 'UsernameAlreadyExist'})
 
+@app.route('/register/biometrics', methods=['POST'])
+def biometrics():
+    if request.method == 'POST':
+        response = dict(request.get_json())
+        user_id = response['user_id']
+        data = response['data']
+        data.append(user_id)  # append user id to the end of the list
+        try:
+            with open(TYPING_DATA_PATH, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
 
-@app.route('/cadastro/biometria', methods = ['POST'])
-def biometria():
-	if request.method == 'POST':
-		response = dict(request.get_json())
-		user_id  = response['user_id']
-		data = response['data']
-		data.append(user_id) # adiciona o user id ao fim da lista
-		try:
-			with open(TYPING_DATA_PATH, 'a', newline='') as file:			
-				writer = csv.writer(file)
-				writer.writerow(data)
+            return jsonify({'biometric_code': 'Success'})
+        except:
+            return jsonify({'biometric_code': 'Unable to register biometric data'})
 
-			return jsonify({'biometric_cod': 'Success'})
-		except:
-			return jsonify({'biometric_cod': 'Unable to register biometric data'})
+@app.route('/train/biometrics', methods=['POST'])
+def train_biometrics():
+    if request.method == 'POST':
+        response = dict(request.get_json())
+        username = response['username']
+        data = response['data']
+        user_id = get_user_id(username)
+        if user_id is None:
+            user_id = 999  # If user not registered for training, data can still be used.
+        data.append(user_id)  # add user id to the end of the list
+        try:
+            with open(TYPING_DATA_PATH, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
 
-@app.route('/treinar/biometria', methods = ['POST'])
-def treinar():
-	if request.method == 'POST':
-		response = dict(request.get_json())
-		username  = response['username']
-		data = response['data']
-		user_id = get_user_id(username)
-		if user_id == None: #If a user not registered in the training is entered, the data can still be used.
-			user_id = 999 
-		data.append(user_id) # add the user id to the end of the list
-		try:
-			with open(TYPING_DATA_PATH, 'a', newline='') as file:			
-				writer = csv.writer(file)
-				writer.writerow(data)
+            return jsonify({'biometric_code': 'Success'})
+        except:
+            return jsonify({'biometric_code': 'Unable to register biometric data'})
 
-			return jsonify({'biometric_cod': 'Success'})
-		except:
-			return jsonify({'biometric_cod': 'Unable to register biometric data'})
-
-
-@app.route('/login', methods = ['GET'])
+@app.route('/login', methods=['GET'])
 def login():
-	return render_template('./login/login.html')
+    return render_template('./login/login.html')
 
-@app.route('/login/auth1', methods = ['POST']) # Route for first authentication
+@app.route('/login/auth1', methods=['POST'])  # Route for first authentication
 def auth1():
-	response = dict(request.get_json())
-	username  = response['username']
-	password = response['password']
+    response = dict(request.get_json())
+    username = response['username']
+    password = response['password']
 
-	id, result, user_id = check_user_and_passw(username, password)
+    id, result, user_id = check_user_and_passw(username, password)
 
-	if result:
-		return jsonify({'auth1_code': 'success', 'id_usuario': user_id})
-	else:
-		if id == 3:
-			return jsonify({'auth1_code': 'UsernameNotExist'})
-		elif id == 1:
-			return jsonify({'auth1_code': 'PasswordIsWrong'})
+    if result:
+        return jsonify({'auth1_code': 'success', 'user_id': user_id})
+    else:
+        if id == 3:
+            return jsonify({'auth1_code': 'UsernameNotExist'})
+        elif id == 1:
+            return jsonify({'auth1_code': 'PasswordIsWrong'})
 
-@app.route('/login/auth2', methods = ['POST']) # Route for second authentication
+@app.route('/login/auth2', methods=['POST'])  # Route for second authentication
 def auth2():
-	response = dict(request.get_json())
-	amostra_digitacao  = response['typing_data']
-	user_id = response['user_id']
-	
-	##### Classificação
-	classifica = Classificador(TYPING_DATA_PATH, amostra_digitacao, SPLIT, K)
-	resultado = classifica.knn_manhattan_sem_treino()
-	cross_val_score = classifica.get_cv_score()
-
-	if str(user_id) in resultado[0]:
-		match = True
-	else:
-		match = False
-	
-	data_hora_atual = datetime.datetime.now()
-	data_atual = data_hora_atual.strftime("%d/%m/%Y %H:%M:%S ")
+    response = dict(request.get_json())
+    typing_data = response['typing_data']
+    user_id = response['user_id']
     
-	with open(LOG_NAME, 'a') as arquivo: # Create the log file
-		arquivo.write('[+]  Usuario Real: ')
-		arquivo.write(str(user_id))
-		arquivo.write(' | Usuario Previsto: ')
-		arquivo.write(str(resultado[0]))
-		arquivo.write(' | Algoritimo: ')
-		arquivo.write(str(resultado[2]))
-		arquivo.write(' | Valor de K: ')
-		arquivo.write(str(K))
-		arquivo.write(' | Match: ')
-		arquivo.write(str(match))
-		arquivo.write(' | Accuracy: ')
-		arquivo.write(str(cross_val_score))
-		arquivo.write(' | Data: ')
-		arquivo.write(data_atual)
-		arquivo.write('\n')
+    ##### Classification
+    classifier = Classifier(TYPING_DATA_PATH, typing_data, SPLIT, K)
+    result, cross_val_score = classifier.knn_manhattan_without_training()
 
-	return jsonify({'user_id':str(user_id), 'predict': resultado[0], 'accuracy': cross_val_score, 'result': str(match), 'algoritimo': resultado[2]})
+    if str(user_id) in result[0]:
+        match = True
+    else:
+        match = False
+    
+    current_datetime = datetime.datetime.now()
+    current_date = current_datetime.strftime("%d/%m/%Y %H:%M:%S ")
+    
+    with open(LOG_NAME, 'a') as file:  # Create the log file
+        file.write('[+]  Real User: ')
+        file.write(str(user_id))
+        file.write(' | Predicted User: ')
+        file.write(str(result[0]))
+        file.write(' | Algorithm: ')
+        file.write(str(result[2]))
+        file.write(' | Value of K: ')
+        file.write(str(K))
+        file.write(' | Match: ')
+        file.write(str(match))
+        file.write(' | Accuracy: ')
+        file.write(str(cross_val_score))
+        file.write(' | Date: ')
+        file.write(current_date)
+        file.write('\n')
 
-@app.route('/treinar', methods = ['GET', 'POST'])
-def treina_bio():
-	if request.method == 'GET':
-		return render_template('./treinamento/treinamento.html')
+    return jsonify({'user_id': str(user_id), 'predict': result[0], 'accuracy': cross_val_score, 'result': str(match), 'algorithm': result[2]})
 
-@app.route('/best_params', methods = ['GET'])
+@app.route('/train', methods=['GET', 'POST'])
+def train_biometrics_page():
+    if request.method == 'GET':
+        return render_template('./training/training.html')
+
+@app.route('/best_params', methods=['GET'])
 def best_params():
-	return render_template('./best_params/best_params.html')
+    return render_template('./best_params/best_params.html')
 
-@app.route('/best_params/result', methods = ['GET'])
+@app.route('/best_params/result', methods=['GET'])
 def best_params_result():
-	amostra_digitacao = '' 
-	classifica = Classificador(TYPING_DATA_PATH, amostra_digitacao, 0.7, 3)
-	best_score, best_params, best_estimator = classifica.hyper_parameters_tuning()
+    typing_data = ''
+    classifier = Classifier(TYPING_DATA_PATH, typing_data, 0.7, 3)
+    best_score, best_params, best_estimator = classifier.hyper_parameters_tuning()
 
-	data_hora_atual = datetime.datetime.now()
-	data_atual = data_hora_atual.strftime("%d/%m/%Y %H:%M:%S ")
+    current_datetime = datetime.datetime.now()
+    current_date = current_datetime.strftime("%d/%m/%Y %H:%M:%S ")
 
-	with open(LOG_NAME, 'a') as arquivo: # Create the log file
-		arquivo.write('[+]  Best Score: ')
-		arquivo.write(str(best_score))
-		arquivo.write(' |  Best Params: ')
-		arquivo.write(str(best_params))
-		arquivo.write(' |  Best Estimator: ')
-		arquivo.write(str(best_estimator))
-		arquivo.write(' | Data: ')
-		arquivo.write(data_atual)
-		arquivo.write('\n')
+    with open(LOG_NAME, 'a') as file:  # Create the log file
+        file.write('[+]  Best Score: ')
+        file.write(str(best_score))
+        file.write(' |  Best Params: ')
+        file.write(str(best_params))
+        file.write(' |  Best Estimator: ')
+        file.write(str(best_estimator))
+        file.write(' | Date: ')
+        file.write(current_date)
+        file.write('\n')
 
-	return jsonify({'best_score':str(best_score), 'best_params': str(best_params), 'best_estimator': str(best_estimator) })
-	
+    return jsonify({'best_score': str(best_score), 'best_params': str(best_params), 'best_estimator': str(best_estimator) })
+
 # Server Start
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', debug=True, port=3000)
+    app.run(host='127.0.0.1', debug=True, port=3000)
