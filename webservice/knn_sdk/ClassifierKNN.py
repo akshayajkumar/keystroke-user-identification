7b93603b520d0aed4d0b7f028fec1a50bba4c6ee
akshayajkumar <145012137+akshayajkumar@users.noreply.github.com>
2024-08-06 18:54:18 +0530

    Update ClassifierKNN.py

--- webservice/knn_sdk/ClassifierKNN.py
+++ webservice/knn_sdk/ClassifierKNN.py
@@ -1,26 +1,23 @@
-
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-# teste
+# test
 from sklearn.model_selection import cross_validate
 
-
-
-class Classificador:
-    def __init__(self, arquivo_biometrico_cadastrados, amostra_digitacao, knn_model_test_ratio, neighbour_size):
-        self.arquivo_biometrico_cadastrados = arquivo_biometrico_cadastrados    
-        self.amostra_digitacao = amostra_digitacao                              
+class Classifier:
+    def __init__(self, registered_biometric_file, typing_sample, knn_model_test_ratio, neighbor_size):
+        self.registered_biometric_file = registered_biometric_file    
+        self.typing_sample = typing_sample                              
         self.knn_model_test_ratio = knn_model_test_ratio                        
-        self.neighbour_size = neighbour_size                                   
+        self.neighbor_size = neighbor_size                                   
 
     def knn_manhattan_holdout(self): 
         description = 'knn_manhattan_t'
-        keystroke_data = pd.read_csv(self.arquivo_biometrico_cadastrados, keep_default_na=False)
+        keystroke_data = pd.read_csv(self.registered_biometric_file, keep_default_na=False)
         
-        amostra = self.amostra_digitacao
+        sample = self.typing_sample
 
         # Must be changed when the array quantity is different from 121
         data = keystroke_data.iloc[:, 0:97]  
@@ -28,56 +25,56 @@ class Classificador:
         # Classes for application in supervised learning
         target = keystroke_data['CLASS']
 
-        dataframe_amostra = pd.DataFrame.transpose(pd.DataFrame(amostra))
+        dataframe_sample = pd.DataFrame.transpose(pd.DataFrame(sample))
 
         data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=self.knn_model_test_ratio, random_state=10)
 
-        knn_model = KNeighborsClassifier(n_neighbors=self.neighbour_size, metric="manhattan")
+        knn_model = KNeighborsClassifier(n_neighbors=self.neighbor_size, metric="manhattan")
 
         knn_model.fit(data_train, target_train)
 
         inner_prediction = knn_model.predict(data_test)
-        usuario_predict = knn_model.predict(dataframe_amostra)
+        user_predict = knn_model.predict(dataframe_sample)
 
         accuracy = accuracy_score(target_test, inner_prediction)
 
-        print("[+] User predict:", usuario_predict)
+        print("[+] User predict:", user_predict)
         
-        return str(usuario_predict), str(accuracy), description
+        return str(user_predict), str(accuracy), description
 
         
-    def knn_manhattan_sem_treino(self):
+    def knn_manhattan_no_training(self):
         description = 'knn_manhattan_s'
-        keystroke_data = pd.read_csv(self.arquivo_biometrico_cadastrados, keep_default_na=False)
+        keystroke_data = pd.read_csv(self.registered_biometric_file, keep_default_na=False)
         
-        amostra = self.amostra_digitacao
+        sample = self.typing_sample
 
-       # Must be changed when the array quantity is different from 121
+        # Must be changed when the array quantity is different from 121
         data = keystroke_data.iloc[:, 0:97]  
 
         # Classes for application in supervised learning
         target = keystroke_data['CLASS']
 
-        sample_text_row = pd.DataFrame.transpose(pd.DataFrame(amostra))
+        sample_text_row = pd.DataFrame.transpose(pd.DataFrame(sample))
 
-        knn_model = KNeighborsClassifier(n_neighbors=self.neighbour_size, metric="manhattan")
+        knn_model = KNeighborsClassifier(n_neighbors=self.neighbor_size, metric="manhattan")
 
         knn_model.fit(data, target)
 
         predict_label = []
-        for n in range(0,len(target)):
+        for n in range(0, len(target)):
             inner_prediction = knn_model.predict(sample_text_row)
             predict_label.append(inner_prediction)
     
-        acuracia = accuracy_score(target, predict_label)
+        accuracy = accuracy_score(target, predict_label)
 
         print('[+] User Predict - ', inner_prediction)
  
-        return str(inner_prediction), str(acuracia), description
+        return str(inner_prediction), str(accuracy), description
 
     def hyper_parameters_tuning(self):
 
-        keystroke_data = pd.read_csv(self.arquivo_biometrico_cadastrados, keep_default_na=False)
+        keystroke_data = pd.read_csv(self.registered_biometric_file, keep_default_na=False)
         
         # Must be changed when the array quantity is different from 121
         data = keystroke_data.iloc[:, 0:97]  
@@ -85,31 +82,31 @@ class Classificador:
         # Classes for application in supervised learning
         target = keystroke_data['CLASS']
 
-        k_range = list(range(1,10))
-        leaf_size = list(range(1,50))
+        k_range = list(range(1, 10))
+        leaf_size = list(range(1, 50))
         weight_options = ["uniform", "distance"]
-        p=[1,2]
+        p = [1, 2]
 
         param_grid = dict(leaf_size=leaf_size, n_neighbors=k_range, weights=weight_options, p=p)
 
         knn = KNeighborsClassifier()
 
         grid = GridSearchCV(knn, param_grid, scoring='accuracy')
-        grid.fit(data,target)
+        grid.fit(data, target)
 
         best_score = grid.best_score_
         best_params = grid.best_params_
         best_estimator = grid.best_estimator_
 
-        print('[+] Best Score - ',best_score)
-        print('[+] Best Params - ',best_params)
-        print('[+] Best Estimator - ',best_estimator)
+        print('[+] Best Score - ', best_score)
+        print('[+] Best Params - ', best_params)
+        print('[+] Best Estimator - ', best_estimator)
  
         return best_score, best_params, best_estimator
 
     def get_cv_score(self):
-        description = 'knn_manhattan_score_teste'
-        keystroke_data = pd.read_csv(self.arquivo_biometrico_cadastrados, keep_default_na=False)
+        description = 'knn_manhattan_score_test'
+        keystroke_data = pd.read_csv(self.registered_biometric_file, keep_default_na=False)
 
         # Must be changed when the array quantity is different from 121
 
@@ -118,14 +115,12 @@ class Classificador:
         # Classes for application in supervised learning
         target = keystroke_data['CLASS']
 
-        knn_model = KNeighborsClassifier(n_neighbors=self.neighbour_size, metric="manhattan")
+        knn_model = KNeighborsClassifier(n_neighbors=self.neighbor_size, metric="manhattan")
 
         knn_model.fit(data, target)
 
         scores = cross_validate(knn_model, data, target, scoring=['accuracy']) # ROC_AUC
         score_result = scores['test_accuracy'].mean() * 100
-        print('[+] Média Accuracy (test_accuracy): %.2f' % score_result)
+        print('[+] Average Accuracy (test_accuracy): %.2f' % score_result)
  
         return score_result
-
-    
